e7bd45d68bdfd56a37796e249f85bbf092cfd14f
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-18 12:56:37 -0700

    class MelonType is done, fixing make_melon_types

--- harvest.py
+++ harvest.py
@@ -9,6 +9,12 @@ class MelonType(object):
     def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
                  name):
         """Initialize a melon."""
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
         self.pairings = []
 
@@ -16,28 +22,45 @@ class MelonType(object):
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
+        self.pairings.append(pairing)
 
-        # Fill in the rest
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
+        self.code = new_code
 
-        # Fill in the rest
 
+# def make_melon_types(code, name, first_harvest, color, is_seedless, pairing, is_bestseller):
+#     """Returns a list of current melon types."""
+
+#     all_melon_types = []
+
+#     code = MelonType(code, first_harvest, color, is_seedless, is_bestseller, name)
+
+#     pairing = (code, pairing)
+
+#     code.add_pairing(pairing)
+
+#     all_melon_types.append(code)
+
+#     return all_melon_types
 
 def make_melon_types():
     """Returns a list of current melon types."""
 
     all_melon_types = []
 
-    # Fill in the rest
+    melon_instance = MelonType()
+    melon_instance.pairtings
 
     return all_melon_types
+
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
-    # Fill in the rest
+    for melon in melon_types:
+        print(f"{melon.name} pairs with {melon.pairings}")
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
